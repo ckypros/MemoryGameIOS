82ac429d2eb21f49ca7966d2cfa2f21964114f77
ckypros <ckypros@gmail.com>
2021-10-17 15:16:33 -0700

    added oneAndOnly extension to Array, optimized code of indexOfFaceUpCard

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,28 +10,27 @@ import Foundation
 struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
-    private var indexOfFaceUpCard: Int?
+    // newValue is the assigned value
+    private var indexOfFaceUpCard: Int? {
+        get { cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) } }
+    }
     
     mutating func choose (_ card: Card) {
         // the , in if let is operates like an &&, except after && would put chodenIndex out of scope
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
-            !cards[chosenIndex].isFaceUp,
-            !cards[chosenIndex].isMatched
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
         {
             if let potentialMatchIndex = indexOfFaceUpCard {
                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
                     cards[chosenIndex].isMatched = true
                     cards[potentialMatchIndex].isMatched = true
                 }
-                indexOfFaceUpCard = nil
+                cards[chosenIndex].isFaceUp = true
             } else {
-                for index in cards.indices {
-                    cards[index].isFaceUp = false
-                }
                 indexOfFaceUpCard = chosenIndex
             }
-            
-            cards[chosenIndex].isFaceUp.toggle()
         }
     }
     
@@ -49,5 +48,12 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         var isMatched = false
         var content: CardContent
         var id: Int
+    }
+}
+
+
+extension Array {
+    var oneAndOnly: Element? {
+        return (count == 1) ? first : nil
     }
 }
