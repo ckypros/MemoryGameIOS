7ffa68e20bec5e3a8f11906d1dd5066be3cfe13b
ckypros <ckypros@gmail.com>
2021-10-23 00:20:09 -0700

    added Cardify for displaying cards, and added animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -68,32 +68,28 @@ struct CardView: View {
     let card: EmojiMemoryGame.Card
     
     var body: some View {
-        GeometryReader(content: { geometry in
+        GeometryReader { geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 20)).padding(5).opacity(0.5)
-                    Text(card.content).font(iconFont(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(DrawingConstants.zeroOpacity)
-                } else {
-                    shape.fill()
-                }
+                Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 20))
+                    .padding(5)
+                    .opacity(0.5)
+                Text(card.content)
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
-        })
+            .cardify(isFaceUp: card.isFaceUp)
+        }
     }
     
-    private func iconFont(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.iconScale)
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.iconScale)
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let iconScale: CGFloat = 0.75
-        static let zeroOpacity: Double = 0
+        static let fontSize: CGFloat = 32
     }
 }
 
