c194bd8cd0b8ed4ad55658884ce4b1a37125a7df
ckypros <ckypros@gmail.com>
2021-10-23 00:27:36 -0700

    fixed content to always be loaded to fix animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -74,7 +74,7 @@ struct CardView: View {
                     .padding(5)
                     .opacity(0.5)
                 Text(card.content)
-                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))  // animate on match
                     .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
                     .font(Font.system(size: DrawingConstants.fontSize))
                     .scaleEffect(scale(thatFits: geometry.size))
